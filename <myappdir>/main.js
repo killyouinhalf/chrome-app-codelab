ffaa1538af29059e815a33fdfa5199b12af5096d
killyouinhalf <klabeau@gmail.com>
2014-08-12 14:20:45 -0400

    Commit for ST 2

--- <myappdir>/main.js
+++ <myappdir>/main.js
@@ -10,7 +10,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   });
   chrome.app.window.create('index.html', {
     width: 400,
-    height: 400,
+    height: 250,
     resizable: false
   });
 });
